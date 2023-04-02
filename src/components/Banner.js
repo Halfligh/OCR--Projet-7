ce9fb5203f8fc238dfc00ca286ab6377c9d15830
Sebastien Hartmann <coach.habitudes.apprentissages@gmail.com>
2023-04-03 00:27:06 +0200

    add dynamic underline to nav

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,18 +1,20 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 import '../styles/Banner.css'
 import Logo from '../assets-figma/LOGO.svg'
 
 function Banner() {
-    return ( 
+  const location = useLocation();
+
+  return ( 
     <div className='kasa-banner'>
-        <img src={Logo} alt={'Logo'} className='logo'></img>
-        <nav classname='navigation-ul'>
-            <Link classname='navigation-li' to="/" >Accueil</Link>
-            <Link to="/about">À propos</Link>
-        </nav>
+      <img src={Logo} alt={'Logo'} className='logo'></img>
+      <nav classname='navigation-ul'>
+        <Link className={`navigation-li ${location.pathname === "/" ? "active" : ""}`} to="/">Accueil</Link>
+        <Link className={`navigation-li ${location.pathname === "/about" ? "active" : ""}`} to="/about">À propos</Link>
+      </nav>
     </div>
-    )
+  )
 }
 
 export default Banner
