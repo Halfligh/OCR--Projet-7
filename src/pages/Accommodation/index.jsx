9b6636384abf26f266ba55258cff23d4d33ac424
Sebastien Hartmann <coach.habitudes.apprentissages@gmail.com>
2023-04-04 19:17:35 +0200

    add star counter function

--- src/pages/Accommodation/index.jsx
+++ src/pages/Accommodation/index.jsx
@@ -2,9 +2,14 @@ import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 import '../../styles/App.css';
+import '../../styles/Accommodation.css'
 import Banner from '../../components/Banner'
 import Footer from '../../components/Footer'
 import SlideShow from '../../components/Slideshow';
+
+import redStar from '../../assets-figma/red-star.svg'
+import greyStar from '../../assets-figma/grey-star.svg'
+
 import data from '../../data/logements.json'
 
 
@@ -31,6 +36,29 @@ function Accommodation({ pageInfo }) {
   <div className='layout'>
     <Banner />
     <SlideShow pictures={kasa.pictures} alt='Logement' height={415}/>
+    <section className='top-section'>
+        <div>
+          <h1 className='kasa-title'>{kasa.title}</h1>
+          <h2 className='sub-title'>{kasa.location}</h2>
+          <p>tags</p>
+        </div>
+        <div className='host-wrap'>
+          <div className='host'>
+            <p className='host-name'>{kasa.host.name}</p>
+            <img className ='host-pic' src={kasa.host.picture} alt='host portrait'></img>
+          </div>
+          { Array.from({ length: parseInt(kasa.rating)}, (_, index) => (
+              <img key={index} className='star' src={redStar} alt='étoile rouge'/>
+            ))}
+          {Array.from({ length: 5 - parseInt(kasa.rating)}, (_, index) => (
+              <img key={index} className='star' src={greyStar} alt='étoile grise'/> 
+          ))}
+        </div>
+    </section>
+    <section>
+    
+    </section>
+    
     <Footer />
   </div>
   )
