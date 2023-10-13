659349de7edebba2ed54ae2f89aa6cd526522ada
Sebastien Hartmann <coach.habitudes.apprentissages@gmail.com>
2023-10-13 22:55:34 +0200

    suppress warning due to import

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import PageInfo from './utils/Pages-infos';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import PageInfo from "./utils/Pages-infos";
 
-import './styles/index.css'
-import Home from './pages/Home';
-import Accommodation from './pages/Accommodation';
-import About from './pages/About';
-import Error from './pages/Error';
+import "./styles/index.css";
+import Home from "./pages/Home";
+import Accommodation from "./pages/Accommodation";
+import About from "./pages/About";
+import Error from "./pages/Error";
 
 const pageInfo = {
   home: PageInfo.home,
@@ -16,18 +21,21 @@ const pageInfo = {
   accommodation: PageInfo.accommodation,
 };
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 
 root.render(
-    <React.StrictMode>
+  <React.StrictMode>
     <Router>
-        <Routes>
-            <Route path="/" element={<Home pageInfo={pageInfo.home} />} />
-            <Route path="/accommodation/:id" element={<Accommodation pageInfo={pageInfo.accommodation} />} />
-            <Route path="/about" element={<About pageInfo={pageInfo.about} />} />
-            <Route path="/error" element={<Error pageInfo={pageInfo.error} />} />
-            <Route path="*" element={<Navigate to="/error" replace />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home pageInfo={pageInfo.home} />} />
+        <Route
+          path="/accommodation/:id"
+          element={<Accommodation pageInfo={pageInfo.accommodation} />}
+        />
+        <Route path="/about" element={<About pageInfo={pageInfo.about} />} />
+        <Route path="/error" element={<Error pageInfo={pageInfo.error} />} />
+        <Route path="*" element={<Navigate to="/error" replace />} />
+      </Routes>
     </Router>
-</React.StrictMode>
+  </React.StrictMode>
 );
