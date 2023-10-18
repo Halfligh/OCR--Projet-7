777e7584a264e757042bb0d08122f3b860ff0196
Sebastien Hartmann <coach.habitudes.apprentissages@gmail.com>
2023-10-18 18:59:44 +0200

    fix cards responsive on big screen

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,31 +1,44 @@
-import { Link } from 'react-router-dom';
-import '../styles/Card.css'
+import { Link } from "react-router-dom";
+import "../styles/Card.css";
 
 function Card({ id, cover, title }) {
-
-// Modules CSS
+  //Modules CSS
   const cardWrapper = {
-    position: 'relative',
-    width: '100%',
-    height: '100%',
+    position: "relative",
+    width: "100%",
+    height: "100%",
     borderRadius: 10,
-    margin: window.innerWidth < 599 ? 0 : 10, 
-    zIndex : 2,
-  }
+    margin: window.innerWidth < 599 ? 0 : 10,
+    zIndex: 2,
+  };
 
   const linkWrapper = {
-    marginBottom: '30px',
-    width: window.innerWidth < 599 ? '100%' : window.innerWidth < 1024 ? window.innerWidth / 3 : window.innerWidth / 4,
-    height: window.innerWidth < 599 ? 225 : window.innerWidth < 1024 ? window.innerWidth / 3 : window.innerWidth / 4,
+    marginBottom: "30px",
+    width:
+      window.innerWidth < 599
+        ? "100%"
+        : window.innerWidth < 1024
+        ? window.innerWidth / 3
+        : window.innerWidth < 1441
+        ? window.innerWidth / 4
+        : 380,
+    height:
+      window.innerWidth < 599
+        ? 225
+        : window.innerWidth < 1024
+        ? window.innerWidth / 3
+        : window.innerWidth < 1441
+        ? window.innerWidth / 4
+        : 380,
   };
 
   return (
-    <Link style= {linkWrapper} to={`/accommodation/${id}`}>
+    <Link style={linkWrapper} to={`/accommodation/${id}`}>
       <div style={cardWrapper}>
-        <div className='opacity-gradient'/>
-        <img className='card-img' src={cover} alt={title} />
-        <div className='card-bottom'>
-          <h3 className='card-title'>{title}</h3>
+        <div className="opacity-gradient" />
+        <img className="card-img" src={cover} alt={title} />
+        <div className="card-bottom">
+          <h3 className="card-title">{title}</h3>
         </div>
       </div>
     </Link>
